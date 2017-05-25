4aa7aea872fec4c1b31f9c5cc8d8855d723b292b
Brian Gulbis <brian.gulbis@gmail.com>
2017-05-25 13:54:24 +0000

    import aws.s3 from cran

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-    repos = getOption("cloudyr")
-    repos["cloudyr"] = "http://cloudyr.github.io/drat"
-    options(repos = repos)
-    invisible(repos)
-}
