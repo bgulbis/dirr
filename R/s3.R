bf8320eb65591c8e06b70173b84fb1b65f7436be
Brian Gulbis <brian.gulbis@gmail.com>
2017-05-25 19:23:28 +0000

    add function to read all files from s3 bucket dir

--- R/s3.R
+++ R/s3.R
@@ -19,3 +19,30 @@ save_rds_s3 <- function(x, bucket, nm, s3_dir = "data/", encrypt = TRUE) {
                       bucket = bucket,
                       headers = hdrs)
 }
+
+#' Read in RDS files from AWS S3
+#'
+#' \code{get_rds} reads in all RDS files from a directory in an AWS S3 bucket
+#'
+#' This function reads in all RDS files in a given directory in an AWS S3 bucket
+#' and saves them as objects in the Global Environment.
+#'
+#' @param bucket character string, bucket to read data from
+#' @param s3_dir character string, retrieves all files from the specified bucket directory
+#' @param file_ext An optional character string, defaults to .Rds
+#'
+#' @return An \code{R} object
+#'
+#' @seealso \code{\link[aws.s3]{s3readRDS}}
+get_rds_s3 <- function(bucket, s3_dir, file_ext = ".Rds") {
+    files <- aws.s3::get_bucket_df(bucket)
+    files <- dplyr::filter_(files, .dots = list(~stringr::str_detect(Key, s3_dir)))
+
+    raw <- files$Key
+    nm <- stringr::str_replace_all(raw, s3_dir, "")
+    nm <- stringr::str_replace_all(nm, file_ext, "")
+
+    y <- purrr::map(raw, aws.s3::s3readRDS, bucket = bucket)
+    names(y) <- nm
+    list2env(y, .GlobalEnv)
+}
